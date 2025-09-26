5a2a19bbad75650ab6ca2b4797cf39a33242b48d
Студент <@LDPK.local>
2025-09-26 11:01:24 +0300

    создали класс Library и метод addBook

---
+++ my_library.js
@@ -0,0 +1,42 @@
+class Library{
+    constructor(libTitle) {
+        this.name = libTitle;
+        this.books = [];
+        this.totalBooks = 0; //Общее кол во 
+        this.borrowedBooks = 0; // Кол-во выданных
+    }
+
+    // Добавление новой книги. Если 'isbn' уже есть в базе
+    // То увеличим кол - во книг (в поле quantite)
+    addBook(title, author, year, isbn, quantity = 1){
+        const newBook = {
+            title: title,
+            author: author,
+            year: year,
+            isbn: isbn,
+            totalQuantity: quantity, // Кол-во экземпляров
+            avilableQuantity: quantity, // кол-во доступных для выдачи
+            borrowedBy: [] // Кому выдавали и когда 
+        }
+        const findBook = this.books.filter(book => book.isbn == isbn);
+        if (findBook.length){
+            for (var i =0; i<= this.books.length; i++) {
+                if (book in this.books[i] == findBook[0]) {
+                this.books[i]. totalQuantity += quantity;
+                this.books[i]. avilableQuantity += quantity;
+                }
+            }
+        } else{
+            this.books.push(newBook)
+        }
+        return newBook;
+    }
+}
+
+const library = new Library("Тестовая библиотека")
+
+
+// Добавление книг
+library.addBook("Преступление и наказание", "Федор Достоевский", 1866, "978-5-12345-678-9", 3);
+library.addBook("Мастер и Маргарита", "Михаил Булгаков", 1967, "978-5-12345-679-6", 2);
+console.log(library.books);
